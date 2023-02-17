54dc114037717cb710e695f4c99bf83d55b32ee7
github.arief <78076941+arief-github@users.noreply.github.com>
2023-02-17 10:56:41 +0700

    add : designing product detail page

--- src/pages/productDetail/index.jsx
+++ src/pages/productDetail/index.jsx
@@ -1,12 +1,46 @@
 import React from 'react';
+import { products } from '../../data/products.js';
 
 export default function ProductDetail({ match }) {
 	const productId = match.params.id;
 
+	const product = products.find(product => product.id == productId);
+
 	return (
-		<div>
-			<h1 className="text-center">Product Detail Page</h1>
-			<p>Id product : { productId }</p>
+		<div className="row">
+			<div className="col-md-6">
+				<div className="card p-2 m-2"> 
+					<h1>{ product.name }</h1>
+					<img src={product.image} alt="photo product" className="img-fluid m-3 bigimg"/>
+					<p className="text-md-start">{product.description}</p>	
+				</div>
+			</div>
+			<div className="col-md-6">
+				<div className="m-2">
+					<h1>Price : { product.price }</h1>
+
+					<hr/>
+
+					<h1>Select Quantity</h1>
+
+					<select>
+
+						{
+						
+							product.countInStock === 0 ? (<option value="">Out of Stock</option>) :						
+
+							[...Array(product.countInStock).keys()].map((value, index) => {
+								return <option value={index + 1}>{index + 1}</option>
+							})
+
+						}						
+					</select>
+
+					<hr/>
+
+					<button className="btn btn-dark text-uppercase">Add to Cart</button>
+				</div>
+			</div>
 		</div>
 	)
 }
