02fdfbd99cc67ba27aae411a825aab000cdfc676
github.arief <78076941+arief-github@users.noreply.github.com>
2023-03-01 22:19:02 +0700

    adding cart page

---
+++ src/reducer/cartReducer.js
@@ -0,0 +1,19 @@
+const addToCartReducer = (state = { cartItems: [] }, action) => {
+    switch (action.type) {
+        case 'ADD_TO_CART':
+            const alreadyExist = state.cartItems.find(item => item._id == action.payload._id);
+
+            if (alreadyExist) {
+                return {
+                    ...state,
+                    cartItems: state.cartItems.map(item => item._id == action.payload._id ? action.payload : item)
+                }
+            } else {
+                return { ...state, cartItems: [...state.cartItems, action.payload] }
+            }
+        default:
+            return state;
+    }
+}
+
+export default addToCartReducer;
