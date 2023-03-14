5da1b9286c79b9273eb70d37f7c778779f97dad6
Arief Kurniawan <78076941+arief-github@users.noreply.github.com>
2023-03-14 21:05:50 +0700

    update checkout components
    add state loading, error and success

--- src/components/Checkout/index.jsx
+++ src/components/Checkout/index.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import StripeCheckout from 'react-stripe-checkout';
 import { placeOrder } from '../../action/orderActions';
+import Error from '../Common/Error';
+import Loading from '../Common/Loading';
+import Success from '../Common/Success';
 
 const baseStripeKey = import.meta.env.VITE_STRIPE_BASE_URL;
 
 export default function Checkout({ amount }) {
 
   const dispatch = useDispatch();
+  const orderstate = useSelector(state => state.placeOrderReducer)
+
+  const { loading, success, error } = orderstate;
 
    // token handler
    const tokenHandler = (token) => {
@@ -16,6 +22,10 @@ export default function Checkout({ amount }) {
 
   return (
     <div>
+        {loading && (<Loading/>)}
+        {success && (<Success success='Your Order Placed Successfully'/>)}
+        {error && (<Error error='Something went wrong'/>)}
+
         <StripeCheckout
             token={tokenHandler}
             amount={amount * 100}
